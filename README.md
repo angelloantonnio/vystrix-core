75e781e55352e75dcde06492db5f69c292c542d9
Angelo Antonio <angelio123@hotmail.com>
2025-03-12 11:38:16 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+vystrix-core
---
+++ src/main/java/com/vystrix/core/VystrixApplication.java
@@ -0,0 +1,13 @@
+package com.vystrix.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VystrixApplication.class, args);
+	}
+
+}
