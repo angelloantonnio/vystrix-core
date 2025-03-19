55cd4d5c233cd7deba06ecbf63780a125337ced1
Angelo Antonio <angelio123@hotmail.com>
2025-03-18 23:48:06 -0300

    feat(controller): adicionar endpoints para usuários e transações
    - Criado endpoint POST /users no UserController para criar usuários.
    - Criado endpoint POST /transactions no TransactionController para criar transações.
    - Respostas padronizadas utilizando DTOs.

--- src/main/java/com/vystrix/core/adapters/controllers/TransactionController.java
+++ src/main/java/com/vystrix/core/adapters/controllers/TransactionController.java
@@ -2,17 +2,19 @@ package com.vystrix.core.adapters.controllers;
 
 import com.vystrix.core.application.dto.TransactionDTO;
 import com.vystrix.core.application.services.TransactionService;
+import com.vystrix.core.domain.dto.TransactionCreateDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@Validated
 @RequestMapping("/transactions")
 public class TransactionController {
     private final TransactionService transactionService;
@@ -20,5 +22,10 @@ public class TransactionController {
     @GetMapping("/account/{accountId}")
     public ResponseEntity<List<TransactionDTO>> getTransactionByAccountId(@PathVariable Long accountId){
         return ResponseEntity.ok(transactionService.getTransactionByAccountId(accountId));
+    }
+
+    @PostMapping
+    public ResponseEntity<TransactionDTO> createTransaction(@Valid @RequestBody TransactionCreateDTO transactionCreateDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.createTransaction(transactionCreateDTO));
     }
 }
--- src/main/java/com/vystrix/core/adapters/controllers/UserController.java
+++ src/main/java/com/vystrix/core/adapters/controllers/UserController.java
@@ -1,16 +1,19 @@
 package com.vystrix.core.adapters.controllers;
 
+import com.vystrix.core.application.dto.UserAccountDTO;
 import com.vystrix.core.application.dto.UserDTO;
 import com.vystrix.core.application.services.UserService;
+import com.vystrix.core.domain.dto.UserCreateDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+@Validated
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
@@ -18,5 +21,10 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable Long id){
         return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserAccountDTO> createUser(@Valid @RequestBody UserCreateDTO userCreateDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userCreateDTO));
     }
 }
